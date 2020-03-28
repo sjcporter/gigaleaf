ea7b22ef9f3ac0d5e8356193c3fc079697f0a796
Dean Kleissas <dean@gigantum.com>
2020-03-27 21:06:06 -0400

    add auth and update container

---
+++ .circleci/write_auth.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+AUTH_PATH=/home/circleci/project/gigaleaf/tests/resources/secrets.json
+
+echo "{" > ${AUTH_PATH}
+echo "  \"email\": \"${OVERLEAF_EMAIL}\"," >> ${AUTH_PATH}
+echo "  \"password\": \"${OVERLEAF_PASSWORD}\"," >> ${AUTH_PATH}
+echo "  \"git_url\": \"${OVERLEAF_URL}\"," >> ${AUTH_PATH}
+echo "}" >> ${AUTH_PATH}
