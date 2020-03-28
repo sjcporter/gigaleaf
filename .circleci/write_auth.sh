fd8d393e108488a6cd0151c8d9c669d6b77ade49
Dean Kleissas <dean@gigantum.com>
2020-03-27 21:10:23 -0400

    json typo

--- .circleci/write_auth.sh
+++ .circleci/write_auth.sh
@@ -5,5 +5,5 @@ AUTH_PATH=/home/circleci/project/tests/resources/secrets.json
 echo "{" > ${AUTH_PATH}
 echo "  \"email\": \"${OVERLEAF_EMAIL}\"," >> ${AUTH_PATH}
 echo "  \"password\": \"${OVERLEAF_PASSWORD}\"," >> ${AUTH_PATH}
-echo "  \"git_url\": \"${OVERLEAF_URL}\"," >> ${AUTH_PATH}
+echo "  \"git_url\": \"${OVERLEAF_URL}\"" >> ${AUTH_PATH}
 echo "}" >> ${AUTH_PATH}
