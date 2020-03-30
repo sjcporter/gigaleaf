39c6c2d6a600bf065a65650641252f9c8c9ef935
Dean Kleissas <dean@gigantum.com>
2020-03-30 15:50:36 -0400

    config git in circle ci

--- .circleci/setup_circleci.sh
+++ .circleci/setup_circleci.sh
@@ -7,3 +7,7 @@ echo "  \"email\": \"${OVERLEAF_EMAIL}\"," >> ${AUTH_PATH}
 echo "  \"password\": \"${OVERLEAF_PASSWORD}\"," >> ${AUTH_PATH}
 echo "  \"git_url\": \"${OVERLEAF_URL}\"" >> ${AUTH_PATH}
 echo "}" >> ${AUTH_PATH}
+
+
+git config --global user.email "testuser@gigantum.com"
+git config --global user.name "Test User"
